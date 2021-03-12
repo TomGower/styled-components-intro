4b08c708562f0c9e2a45eaa01c07dbf892dc8df1
TomGower <tom.gower@gmail.com>
2021-03-12 14:00:01 -0600

    Set up index & style for switching btwn projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App-content";
+// import App from "./App-content";
+import App from "./App-tracker";
 
 const mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
